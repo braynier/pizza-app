4900f79006db93290316a8fc2579042e51a7f1ba
Mindaugas <mindaugas.jasiunas97@gmail.com>
2024-08-13 19:49:38 +0300

    Tailwind.css styling

--- src/UI/Header.jsx
+++ src/UI/Header.jsx
@@ -1,14 +1,17 @@
 import { Link } from "react-router-dom";
 import SearchOrder from "../features/order/SearchOrder";
+import Username from "../features/user/Username";
 
 function Header() {
   return (
-    <header className="bg-yellow-500">
-      <Link to="/">PizzaGo.</Link>
+    <header className="font-pizza flex items-center justify-between border-b border-stone-200 bg-yellow-500 px-4 py-3 uppercase sm:px-6">
+      <Link className="tracking-widest" to="/">
+        PizzaGo.
+      </Link>
 
       <SearchOrder />
 
-      <p>Jonas</p>
+      <Username />
     </header>
   );
 }
